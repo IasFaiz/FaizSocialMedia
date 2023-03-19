d78e9f01eee2b359b4d49675ae4fd68ba5b759b1
Faiz <faiz@hood.live>
2023-03-19 15:25:28 +0530

    navbar done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,42 @@
 import "./Topbar.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
+
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Faizsocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person sx={{ fontSize: 20 }} />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat sx={{ fontSize: 20 }} />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications sx={{ fontSize: 20 }} />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import Topbar from "../../components/topbar/Topbar";
+
 export default function Home() {
   return (
     <div>
-      <div className="topbar">home</div>
+      <div className="topbar">
+        <Topbar />
+      </div>
     </div>
   );
 }
