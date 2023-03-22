654d5717c581b477d22cd839d4f4015b03786aee
Faiz <faiz@hood.live>
2023-03-22 19:17:47 +0530

    UI Done

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,31 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarcontainer">
+        <div className="birthdaybox">
+          <img src="assets/gift.png" alt="" className="birthdayimage" />
+          <span className="birthdaytext">
+            <b>Virat Kohli</b> and <b>5 others </b>have birthday today.
+          </span>
+        </div>
+
+        <div className="adbox">
+          <img src="assets/ad.png" alt="" className="adimage" />
+        </div>
+
+        <div className="onlinefriends">
+          <span className="onlinefriendtitle">Online Friends</span>
+          <div className="onlinefriendscontainer">
+            <div className="imageandonline">
+              <img src="/assets/person/1.jpeg" alt="" className="onlineimage" />
+              <div className="onlinetag"></div>
+            </div>
+            <span className="onlinefriendname">Faiz Ahmad</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
